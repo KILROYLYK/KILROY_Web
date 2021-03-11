0cfba63e053359aa644379f50ca8e721e099550d
kilroy_lyk <736587340@qq.com>
2021-03-11 15:46:05 +0800

    KILROY

--- Asset/_Global/global.js
+++ Asset/_Global/global.js
@@ -1,16 +1,19 @@
 "use strict";
 // @ts-nocheck
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Authorization = exports.Flip = exports.Popup = exports.Ajax = exports.Preload = exports.Adaptation = exports.Algorithm = exports.FN = exports.Tween = exports.GSAP = exports.Crypto = exports.Console = exports.$ = exports.D = exports.W = void 0;
+exports.Authorize = exports.Flip = exports.Popup = exports.Ajax = exports.Crypto = exports.Preload = exports.Adaptation = exports.Algorithm = exports.FN = exports.Tween = exports.GSAP = exports.Console = exports.$ = exports.D = exports.W = void 0;
+/**
+ * Base
+ */
 exports.W = window, exports.D = document;
 /**
  * Plugin
  */
-exports.$ = require('/usr/local/lib/node_modules/jquery'), exports.Console = require('/usr/local/lib/node_modules/vconsole'), exports.Crypto = require('/usr/local/lib/node_modules/crypto-js'), exports.GSAP = require('/usr/local/lib/node_modules/gsap'), exports.Tween = require('/usr/local/lib/node_modules/@tweenjs/tween.js'); // 过渡
+exports.$ = require('/usr/local/lib/node_modules/jquery'), exports.Console = require('/usr/local/lib/node_modules/vconsole'), exports.GSAP = require('/usr/local/lib/node_modules/gsap'), exports.Tween = require('/usr/local/lib/node_modules/@tweenjs/tween.js'); // 过渡
 /**
  * SDK
  */
-exports.FN = require('../Function/function').default, exports.Algorithm = require('../Algorithm/algorithm').default, exports.Adaptation = require('../Adaptation/adaptation').default, exports.Preload = require('../Preload/preload').default, exports.Ajax = require('../Ajax/ajax').default, exports.Popup = require('../Popup/popup').default, exports.Flip = require('../Flip/flip').default, exports.Authorization = require('../Authorization/authorization').default; // 授权
+exports.FN = require('../Function/function.ts').default, exports.Algorithm = require('../Algorithm/algorithm.ts').default, exports.Adaptation = require('../Adaptation/adaptation.ts').default, exports.Preload = require('../Preload/preload.ts').default, exports.Crypto = require('../Crypto/crypto.ts').default, exports.Ajax = require('../Ajax/ajax.ts').default, exports.Popup = require('../Popup/popup.ts').default, exports.Flip = require('../Flip/flip.ts').default, exports.Authorize = require('../Authorize/authorize.ts').default; // 授权
 /**
  * Global
  */
@@ -21,17 +24,17 @@ var Global = /** @class */ (function () {
     Global.D = exports.D;
     Global.$ = exports.$;
     Global.Console = exports.Console;
-    Global.Crypto = exports.Crypto;
     Global.GSAP = exports.GSAP;
     Global.Tween = exports.Tween;
     Global.FN = exports.FN;
-    // public static readonly Algorithm: any = Algorithm;
+    Global.Algorithm = exports.Algorithm;
     Global.Adaptation = exports.Adaptation;
-    // public static readonly Preload: any = Preload;
+    Global.Preload = exports.Preload;
+    Global.Crypto = exports.Crypto;
     Global.Ajax = exports.Ajax;
     Global.Popup = exports.Popup;
-    // public static readonly Flip: any = Flip;
-    Global.Authorization = exports.Authorization;
+    Global.Flip = exports.Flip;
+    Global.Authorize = exports.Authorize;
     return Global;
 }());
 exports.default = Global;
--- Asset/_Global/global.ts
+++ Asset/_Global/global.ts
@@ -1,31 +1,34 @@
 // @ts-nocheck
 
+/**
+ * Base
+ */
 export const
-    W = window,
-    D = document;
+    W: Window = window,
+    D: Document = document;
 
 /**
  * Plugin
  */
 export const
-    $ = require('/usr/local/lib/node_modules/jquery'), // JQuery
-    Console = require('/usr/local/lib/node_modules/vconsole'), // 日志
-    Crypto = require('/usr/local/lib/node_modules/crypto-js'), // 加密
-    GSAP = require('/usr/local/lib/node_modules/gsap'), // 动效
-    Tween = require('/usr/local/lib/node_modules/@tweenjs/tween.js'); // 过渡
+    $: any = require('/usr/local/lib/node_modules/jquery'), // JQuery
+    Console: any = require('/usr/local/lib/node_modules/vconsole'), // 日志
+    GSAP: any = require('/usr/local/lib/node_modules/gsap'), // 动效
+    Tween: any = require('/usr/local/lib/node_modules/@tweenjs/tween.js'); // 过渡
 
 /**
  * SDK
  */
 export const
-    FN = require('../Function/function').default, // 函数
-    Algorithm = require('../Algorithm/algorithm').default, // 算法
-    Adaptation = require('../Adaptation/adaptation').default, // 适配
-    Preload = require('../Preload/preload').default, // 预加载
-    Ajax = require('../Ajax/ajax').default, // 远程请求
-    Popup = require('../Popup/popup').default, // 弹窗
-    Flip = require('../Flip/flip').default, // 翻页
-    Authorization = require('../Authorization/authorization').default; // 授权
+    FN: any = require('../Function/function.ts').default, // 函数
+    Algorithm: any = require('../Algorithm/algorithm.ts').default, // 算法
+    Adaptation: any = require('../Adaptation/adaptation.ts').default, // 适配
+    Preload: any = require('../Preload/preload.ts').default, // 预加载
+    Crypto: any = require('../Crypto/crypto.ts').default, // 加密
+    Ajax: any = require('../Ajax/ajax.ts').default, // 远程请求
+    Popup: any = require('../Popup/popup.ts').default, // 弹窗
+    Flip: any = require('../Flip/flip.ts').default, // 翻页
+    Authorize: any = require('../Authorize/authorize.ts').default; // 授权
 
 /**
  * Global
@@ -34,18 +37,18 @@ export default class Global {
     public static readonly W: Window = W;
     public static readonly D: Document = D;
     
-    public static readonly $: any = $;
-    public static readonly Console: any = Console;
-    public static readonly Crypto: any = Crypto;
-    public static readonly GSAP: any = GSAP;
-    public static readonly Tween: any = Tween;
+    public static readonly $: typeof $ = $;
+    public static readonly Console: typeof Console = Console;
+    public static readonly GSAP: typeof GSAP = GSAP;
+    public static readonly Tween: typeof Tween = Tween;
     
-    public static readonly FN: any = FN;
-    // public static readonly Algorithm: any = Algorithm;
-    public static readonly Adaptation: any = Adaptation;
-    // public static readonly Preload: any = Preload;
-    public static readonly Ajax: any = Ajax;
-    public static readonly Popup: any = Popup;
-    // public static readonly Flip: any = Flip;
-    public static readonly Authorization: any = Authorization;
+    public static readonly FN: typeof FN = FN;
+    public static readonly Algorithm: typeof Algorithm = Algorithm;
+    public static readonly Adaptation: typeof Adaptation = Adaptation;
+    public static readonly Preload: typeof Preload = Preload;
+    public static readonly Crypto: typeof Crypto = Crypto;
+    public static readonly Ajax: typeof Ajax = Ajax;
+    public static readonly Popup: typeof Popup = Popup;
+    public static readonly Flip: typeof Flip = Flip;
+    public static readonly Authorize: typeof Authorize = Authorize;
 }

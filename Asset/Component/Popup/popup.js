6792a5e67d870428270278454f83c0eda7b32ad1
kilroy_lyk <736587340@qq.com>
2021-03-17 18:42:17 +0800

    KILROY

--- Asset/Component/Popup/popup.js
+++ Asset/Component/Popup/popup.js
@@ -148,7 +148,7 @@ var Popup = /** @class */ (function () {
         if (!_this.popup.toast) {
             _this.popup.toast = new Popup('popup_toast', {
                 content: _this.template.toast,
-                // animation: 'bottom',
+                animation: 'bottom',
                 isScreenClose: true,
                 open: function (data) {
                     _this.popup.toast.$content.find('.content').text(data);

7664b885261c001ddc5cbe86826ee6742f41e9e1
kilroy_lyk <736587340@qq.com>
2021-04-27 17:38:26 +0800

    KILROY

---
+++ Asset/Tool/Gulp/gulpfile.ts
@@ -0,0 +1,21 @@
+// @ts-ignore
+import fs from '/usr/local/lib/node_modules/fs-i';
+// @ts-ignore
+import del from '/usr/local/lib/node_modules/del';
+// @ts-ignore
+import image from '/usr/local/lib/node_modules/imagemin-mozjpeg';
+// @ts-ignore
+import png from '/usr/local/lib/node_modules/imagemin-pngquant';
+// @ts-ignore
+import gulp from '/usr/local/lib/node_modules/gulp-typescript';
+// @ts-ignore
+import gulpZip from '/usr/local/lib/node_modules/gulp-zip';
+// @ts-ignore
+import gulpJsonMinify from '/usr/local/lib/node_modules/gulp-jsonminify';
+// @ts-ignore
+import gulpImagemin from '/usr/local/lib/node_modules/gulp-imagemin';
+
+const path = {
+    file: '',
+    zip: ''
+};
